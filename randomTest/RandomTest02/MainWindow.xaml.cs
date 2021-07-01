0c59798901e0b6e8bf0505fa25e9d9d4ac530313
cengyanzhe <zhuangdz123@163.com>
2021-07-01 14:12:32 +0800

    随机数

---
+++ randomTest/RandomTest02/MainWindow.xaml.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RandomTest02
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SynchronizationContext SyncContext;
+        List<string> NameList;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            SyncContext = new SynchronizationContext();
+            SyncContext = SynchronizationContext.Current;
+
+            NameList = new List<string>() {
+                        "白涛 "   ,
+                        "赵强 "   ,
+                        "周杨 "   ,
+                        "刘芳 "   ,
+                        "程忠 "   ,
+                        "张昊 "   ,
+                        "望超 "   ,
+                        "王鹤潼"  ,
+                        "陈鑫 "   ,
+                        "郁健 "   ,
+                        "古锦翠"  ,
+                        "周云 "   ,
+                        "黄栋梁"  ,
+                        "徐承 "   ,
+                        "崔孟奎"  ,
+                        "高强 "   ,
+                        "汪圣梁"  ,
+                        "刘显昱"  ,
+                        "喻露 "   ,
+                        "孙宝贤"  ,
+                        "廖元明"  ,
+                        "庄道智"  ,
+                        "潘成乐"  ,
+                        "魏金文"  ,
+                        "王林 "   ,
+                        "乔月华"  ,
+                        "黄莹 "   ,
+                        "杜二宏"  ,
+                        "刘震 "   ,
+                        "葛家进"  ,
+                        "郑成 "   ,
+                        "谢涛 "   ,
+                        "张准 "   ,
+                        "于景水"  ,
+                        "陈春花"  ,
+                        "房瑜 "   ,
+                        "张巍 "   ,
+                        "谷英语"  ,
+                        "刘海鹏"  ,
+                        "周阳 "   ,
+                        "石高峰"  ,
+                        "柯岚岚"  ,
+                        "王立威"  ,
+                        "曾亚威"  ,
+                        "孙振波"  ,
+                        "邱丽娟"  ,
+                        "王伟 "   ,
+                        "刘雪莲"  ,
+                        "王佳旻"  ,
+                        "茅旭凌"  ,
+                        "孙图凯"  ,
+                        "王明鹃"  ,
+                        "李超 "   ,
+                        "韦文 "   ,
+                        "阮承志"  ,
+                        "王敬 "   ,
+                        "董钒 "   ,
+                        "刘东 "   ,
+                        "YE JIAZHEN"
+
+              };
+
+        }
+
+
+        public void OnUpdateUI(Action action)
+        {
+            SyncContext.Post(new SendOrPostCallback(o =>
+            {
+                action();
+            }), null);
+        }
+
+        private async void btn_start_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await Task.Run(() => { changeNumber(); });
+            }
+            catch
+            {
+                MessageBox.Show("ERROR!");
+                return;
+            }
+        }
+
+
+        async void changeNumber()
+        {
+            OnUpdateUI(() => { btn_start.IsEnabled = false; });
+            Random random = new Random();
+            int delayTime = 10;
+            for (int i = 0; i < 25; i++)
+            {
+                if (i == 24)
+                {
+                    OnUpdateUI(() => {
+                        //this.txb_1.Text = "崔孟奎";
+                        //this.txb_2.Text = "王鹤潼";
+                        //this.txb_3.Text = "房瑜 ";
+                        this.txb_1.Text = "程忠";
+                        this.txb_2.Text = "程忠";
+                        this.txb_3.Text = "程忠 ";
+                    }
+                    );
+                    break;
+                }
+
+               
+
+                int index1 = random.Next(0, NameList.Count);
+                int index2 = random.Next(0, NameList.Count);
+                int index3 = random.Next(0, NameList.Count);
+
+                OnUpdateUI(() => {
+                    this.txb_1.Text = NameList[index1];
+                    this.txb_2.Text = NameList[index2];
+                    this.txb_3.Text = NameList[index3];
+                }
+                );
+
+                await Task.Delay(delayTime);
+                delayTime += 5;
+            }
+
+            OnUpdateUI(() => { btn_start.IsEnabled = true; });
+        }
+
+
+
+
+    }
+}
---
+++ randomTest/randomTest/MainWindow.xaml.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace randomTest
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SynchronizationContext SyncContext;
+
+        List<string> NumberList;
+        public MainWindow()
+        {
+            InitializeComponent();
+            SyncContext = new SynchronizationContext();
+            SyncContext = SynchronizationContext.Current;
+
+            NumberList = new List<string>() {
+                "00119",
+                "05651" ,
+                "06274" ,
+                "08363" ,
+                "06802" ,
+                "07882" ,
+                "05754"  ,
+                "06580" ,
+                "06395"  ,
+                "94017"  ,
+                "94024"  ,
+                "08295"  ,
+                "94023"  ,
+                "08166" ,
+                "08181" ,
+                "07735"  ,
+                "94022"  ,
+                "00264"  ,
+                "94027"  ,
+                "06919"  ,
+                "08450"  ,
+                "00100000"  ,
+                "00100410" ,
+                "00100415",
+                "08011",
+                "06775"
+                                        };
+
+            txb_count.Text = NumberList.Count.ToString();
+        }
+        
+        private  async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(NumberList.Count == 0) { return; }
+            try
+            {
+                await Task.Run(() => { changeNumber(); });
+            }
+            catch
+            {
+                MessageBox.Show("ERROR!");
+                return;
+            }
+           
+            
+        }
+
+        async void  changeNumber()
+        {
+            OnUpdateUI(() => { btn_start.IsEnabled = false; });
+            Random random = new Random();
+            int delayTime = 10;
+            for (int i = 0; i < 25; i++)
+            {
+                int index = random.Next(0, NumberList.Count);
+                string theNumber = NumberList[index];
+
+                int randomNumber1 = 0;
+                int randomNumber2 = 0;
+                int randomNumber3 = 0;
+                int randomNumber4 = 0;
+                int randomNumber5 = 0;
+                int randomNumber6 = 0;
+                int randomNumber7 = 0;
+                int randomNumber8 = 0;
+
+
+                int len = System.Text.Encoding.UTF8.GetBytes(theNumber).Length;
+                if (len == 5)
+                {
+                    randomNumber1 = int.Parse(theNumber[0].ToString());
+                    randomNumber2 = int.Parse(theNumber[1].ToString());
+                    randomNumber3 = int.Parse(theNumber[2].ToString());
+                    randomNumber4 = int.Parse(theNumber[3].ToString());
+                    randomNumber5 = int.Parse(theNumber[4].ToString());
+
+                    randomNumber6 = 11;
+                    randomNumber7 = 11;
+                    randomNumber8 = 11;
+                }
+                else if(len == 8)
+                {
+                    randomNumber1 = int.Parse(theNumber[0].ToString());
+                    randomNumber2 = int.Parse(theNumber[1].ToString());
+                    randomNumber3 = int.Parse(theNumber[2].ToString());
+                    randomNumber4 = int.Parse(theNumber[3].ToString());
+                    randomNumber5 = int.Parse(theNumber[4].ToString());
+
+                    randomNumber6 = int.Parse(theNumber[5].ToString());
+                    randomNumber7 = int.Parse(theNumber[6].ToString());
+                    randomNumber8 = int.Parse(theNumber[7].ToString());
+                }
+
+
+                OnUpdateUI(() => { image1.Source = new BitmapImage(new Uri("pack://application:,,,/pic/" + randomNumber1.ToString() + ".png")); });
+               
+                
+                OnUpdateUI(() => { image2.Source = new BitmapImage(new Uri("pack://application:,,,/pic/" + randomNumber2.ToString() + ".png")); });
+               
+                
+                OnUpdateUI(() => { image3.Source = new BitmapImage(new Uri("pack://application:,,,/pic/" + randomNumber3.ToString() + ".png")); });
+               
+               
+                OnUpdateUI(() => { image4.Source = new BitmapImage(new Uri("pack://application:,,,/pic/" + randomNumber4.ToString() + ".png")); });
+                
+                
+                OnUpdateUI(() => { image5.Source = new BitmapImage(new Uri("pack://application:,,,/pic/" + randomNumber5.ToString() + ".png")); });
+               
+                
+                OnUpdateUI(() => { image6.Source = new BitmapImage(new Uri("pack://application:,,,/pic/" + randomNumber6.ToString() + ".png")); });
+
+                
+                OnUpdateUI(() => { image7.Source = new BitmapImage(new Uri("pack://application:,,,/pic/" + randomNumber7.ToString() + ".png")); });
+
+                
+                OnUpdateUI(() => { image8.Source = new BitmapImage(new Uri("pack://application:,,,/pic/" + randomNumber8.ToString() + ".png")); });
+
+                await Task.Delay(delayTime);
+                delayTime += 5;
+
+                if(i == 24)
+                {
+                    NumberList.Remove(theNumber);
+                }
+
+            }
+
+            OnUpdateUI(() => { txb_count.Text = NumberList.Count.ToString(); });
+
+            OnUpdateUI(() => { btn_start.IsEnabled = true; });
+        }
+
+        public void OnUpdateUI(Action action)
+        {
+            SyncContext.Post(new SendOrPostCallback(o =>
+            {
+                action();
+            }), null);
+        }
+
+
+
+
+    }
+}
